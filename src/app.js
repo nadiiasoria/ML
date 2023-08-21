f02d1d81ded836577dcec29461bf2327843f6f4c
nadiiasoria <nadiasoria5972@gmail.com>
2023-08-21 11:58:16 -0300

    actualizado

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
+
 
 
 app.listen(process.env.PORT || 3001, () => {
@@ -12,5 +13,13 @@ app.listen(process.env.PORT || 3001, () => {
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/home.html"));
+    res.sendFile(path.join(__dirname, "./views/home.html"));
 });
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/register.html"));
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/login.html"));
+})
